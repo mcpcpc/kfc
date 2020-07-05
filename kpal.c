62b112d4cf05a11a7678f861fa874af4fe76fe7d
mcpcpc <michaelczigler@icloud.com>
2020-07-05 16:43:18 -0400

    update

---
+++ kpal.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include "kpal.h"
+
+int
+select_palette(void)
+{
+}
+
+int
+random_palette
+
+int
+list_palette(void)
+{
+}
+
+int
+print_palette(void)
+{
+}
+
+int
+main(nt argc, char **argv)
+{
+	extern char *optarg;
+	   extern int optind, optopt;
+	p.MODE = "dark";
+}
